d02ba125cddadab3411e059c5849bc4da4a3ae75
chris <christopher.rojas@alumnos.ucn.cl>
2020-12-01 03:42:51 +0100

    test(services/TestContractsImplNewsApi): add test the contracts with news api service

---
+++ app/src/test/java/cl/ucn/disc/dsm/crojas/news/services/TestContractsImplNewsApi.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2020. Christopher Rojas-Garri, christopher.rojas@alumnos.ucn.cl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package cl.ucn.disc.dsm.crojas.news.services;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import cl.ucn.disc.dsm.crojas.news.model.News;
+
+/**
+ * Testing the Contracts with NewsApi service.
+ * @author Christopher Rojas-Garri
+ */
+public class TestContractsImplNewsApi {
+
+    /**
+     * The Logger
+     */
+    private static final Logger log = LoggerFactory.getLogger(TestContractsImplNewsApi.class);
+    /**
+     * The Test
+     */
+    @Test
+    public void testRetrieveNews(){
+     log.debug( "Testing ..");
+        String apiKey = " b7f870ce98c249898a22cf1a244c02bc";
+
+       //The Contracts
+        Contracts contracts = new ContractsImplNewsApi(apiKey);
+
+        //The list of news
+        List<News> news = contracts.retrieveNews(10);
+
+        //Validations.
+        Assertions.assertNotNull(news, "List Null :(");
+        Assertions.assertEquals(10,news.size(),"Wrong size! :(");
+
+       //Show The News.
+        for (News n : news){
+            log.debug("News:{}.", ToStringBuilder.reflectionToString(n, ToStringStyle.MULTI_LINE_STYLE));
+        }
+     log.debug( ".. Done. ");
+   }
+
+}
