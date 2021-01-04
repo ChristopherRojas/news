bfe5d299c2d261e5bb34f5100b44b9e02e082355
chris <christopher.rojas@alumnos.ucn.cl>
2021-01-04 03:13:22 +0100

    fix(ExampleUnitTest): fix to comment

--- app/src/test/java/cl/ucn/disc/dsm/crojas/news/ExampleUnitTest.java
+++ app/src/test/java/cl/ucn/disc/dsm/crojas/news/ExampleUnitTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation example</a>
  */
 public class ExampleUnitTest {
     @Test
