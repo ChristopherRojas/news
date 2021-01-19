1e8ddf1d7baa3a5e024465a8e3f2ee0b6063e02a
chris <christopher.rojas@alumnos.ucn.cl>
2021-01-20 00:41:45 +0100

    feat(Project): implemented fresco image app news

--- app/src/main/java/cl/ucn/disc/dsm/crojas/news/model/NewsItem.java
+++ app/src/main/java/cl/ucn/disc/dsm/crojas/news/model/NewsItem.java
@@ -1,20 +1,22 @@
 /*
  * Copyright (c) 2020. Christopher Rojas-Garri, christopher.rojas@alumnos.ucn.cl
+ * Copyright (c) 2021. Camilo Barrera-Arancibia,camilo.barrera@alumnos.ucn.cl
+ * Copyright (c) 2021. Marcelo Lam-Biagguini,marcelo.lam@alumnos.ucn.cl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
- *  and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
  * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- *  and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
- *  subject to the following conditions:
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
  * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- *  PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
- *  FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- *  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package cl.ucn.disc.dsm.crojas.news.model;
@@ -27,6 +29,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.mikepenz.fastadapter.items.ModelAbstractItem;
 
 import org.threeten.bp.format.DateTimeFormatter;
@@ -90,12 +93,15 @@ public final class NewsItem extends ModelAbstractItem<News, NewsItem, NewsItem.V
     public void bindView(@NonNull ViewHolder holder, @NonNull List<Object> payloads) {
         super.bindView(holder, payloads);
 
+        //Image uri implement
+        // Uri uri = Uri.parse(getModel().getUrlImage()); TODO
         // Setting the holder
         holder.title.setText(getModel().getTitle());
         holder.author.setText(getModel().getAuthor());
         holder.source.setText(getModel().getSource());
         holder.description.setText(getModel().getDescription());
         holder.publishedAt.setText(FORMATTER.format(getModel().getPublishedAt()));
+        holder.image.setImageURI(getModel().getUrlImage());
     }
 
     /**
@@ -124,6 +130,7 @@ public final class NewsItem extends ModelAbstractItem<News, NewsItem, NewsItem.V
         protected TextView source;
         protected TextView description;
         protected TextView publishedAt;
+        protected SimpleDraweeView image;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -132,6 +139,8 @@ public final class NewsItem extends ModelAbstractItem<News, NewsItem, NewsItem.V
             this.source = itemView.findViewById(R.id.in_tv_source);
             this.description = itemView.findViewById(R.id.in_tv_description);
             this.publishedAt = itemView.findViewById(R.id.in_tv_published_at);
+            this.image = itemView.findViewById(R.id.my_image_view);
+
         }
     }
 }
